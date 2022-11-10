20bb15889058a957fd259015674c00533d7fe0fd
Eric Clemente <eeric20011@gmail.com>
2022-11-10 15:07:03 +0100

    Update: code optimized

--- challenge01/index.js
+++ challenge01/index.js
@@ -1,76 +1,46 @@
 import { readFileSync } from "fs";
 
-const VALID_KEYS = ["usr", "eme", "psw", "psw", "age", "loc", "fll"];
+const REQUIRED_KEYS = ["usr", "eme", "psw", "psw", "age", "loc", "fll"];
 
 function main() {
   try {
-    // read the file
-    const data = readFileSync(process.cwd() + "/challenge01/users.txt", "utf8");
-    // Split the users
-    let splitedData = data.split("\r\n");
+    // read the file and split the users
+    const userDataLines = readFileSync(process.cwd() + "/challenge01/users.txt", "utf8").split("\r\n");
 
     // Variables we are going to use
-    let validUsers = 0;
-    let lastCorrectUserName = undefined;
-    let userDataStr = "";
+    const users = [];
+    let tmpUserDataStr = "";
 
     // Loop over the data we have split
-    for (const data of splitedData) {
+    for (const line of userDataLines) {
       // If it's an Empty string, it means that we have all the info of the current user
-      if (isEmptyString(data)) {
+      if (line !== "") {
+        // We need to concat the strings to build all the user data.
+        tmpUserDataStr = tmpUserDataStr !== "" ? tmpUserDataStr.concat(" ", line) : line;
+      } else {
         // Split the user string into keypair strings
         // e.g. => 'fll:111 eme:yrfa@gmail.com usr:@codember' into => ['fll:111','eme:yrfa@gmail.com','usr:@codember'']
-        let user = userDataStr.split(" ");
-
         // After getting the string keypairs, we map the array to transform every keyPairString into an entry
         // e.g. => 'fll:111' into => ['ffl':'111']
-        const entries = user.map((keyPairString) => keyPairString.split(":"));
+        const entries = tmpUserDataStr.split(" ").map((keyPairString) => keyPairString.split(":"));
 
         // Finally, we create our object using the method Object.fromEntries()
         // More info https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/fromEntries
-        user = Object.fromEntries(entries);
+        const user = Object.fromEntries(entries);
 
-        // Check if the object have all the valid keys.
-        if (hasValidKeys(user, VALID_KEYS)) {
-          validUsers++;
-          lastCorrectUserName = user["usr"];
+        // Check if the object have all the required  keys.
+        if (REQUIRED_KEYS.every((key) => Object.hasOwn(user, key))) {
+          users.push(user);
         }
         // Reset the string, so we can build the next user.
-        userDataStr = "";
-      } else {
-        // We need to concat the strings to build all the user data.
-        userDataStr = userDataStr !== "" ? userDataStr.concat(" ", data) : data;
+        tmpUserDataStr = "";
       }
     }
     //After that we should have the count of valid users and the last user name.
-    console.log(validUsers + lastCorrectUserName);
-    //this is for the test, nothing important.
-    return [validUsers, lastCorrectUserName];
+    return [users.length, users.at(-1).usr];
   } catch (error) {
     console.error(`ERROR: ${error}`);
   }
 }
-
-/**
- * Checks if the variables is typeof string, and if its empty.
- * @param {string} str Variable to be checked
- * @returns boolean
- */
-function isEmptyString(str) {
-  return typeof str === "string" && str.trim().length === 0;
-}
-
-/**
- * Checks if all the keys are defined in the object.
- * @param {object} user object to check
- * @param {string[]} keys keys that will be checked
- * @returns boolean
- */
-function hasValidKeys(obj, keys) {
-  return keys.every((key) => {
-    return Object.hasOwn(obj, key);
-  });
-}
-
 main();
 export default main;
