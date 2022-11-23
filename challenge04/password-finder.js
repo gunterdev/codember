e0be4db2ad8245fae220e87456d91d3f5cc55347
Eric Clemente <eeric20011@gmail.com>
2022-11-23 15:18:24 +0100

    Update: refactor code for challenge04

--- challenge04/password-finder.js
+++ challenge04/password-finder.js
@@ -1,18 +1,18 @@
 export const passwordFinder = () => {
   let arr = [];
   for (let value = 11155; value <= 55999; value++) {
-    let isIncreasing = true;
-    let strValue = value.toString();
-    let i = 0;
-    while (i < strValue.length - 1 && isIncreasing) {
-      if (strValue[i] > strValue[i + 1]) {
-        isIncreasing = false;
-      }
-      i++;
-    }
-    if (strValue.includes("55") && isIncreasing) {
+    if (isValid(value)) {
       arr.push(value);
     }
   }
   return arr;
+};
+
+export const isValid = (e) => {
+  let str = e.toString();
+  if (!str.includes("55")) return false;
+  for (e = 0; e < str.length - 1; e++) {
+    if (str[e] > str[e + 1]) return false;
+  }
+  return true;
 };
