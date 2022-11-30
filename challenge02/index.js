09a5a22f5ba3a7dfafcd3274465c368b0092649d
Eric Clemente <eeric20011@gmail.com>
2022-11-30 01:40:09 +0100

    Fix: Useless main function

--- challenge02/index.js
+++ challenge02/index.js
@@ -1,16 +1,13 @@
 import { readFileSync } from "fs";
 import { ASCIIdecryptor } from "./ascii-decryptor.js";
 
-function main() {
-  try {
-    // read the file
-    const encryptedMessage = readFileSync(process.cwd() + "/challenge02/encrypted.txt", "utf8");
-    const decryptedMessage = ASCIIdecryptor(encryptedMessage);
-    console.log(decryptedMessage);
-  } catch (error) {
-    console.error(`ERROR: ${error}`);
-  }
+try {
+  // read the file
+  const encryptedMessage = readFileSync(process.cwd() + "/challenge02/encrypted.txt", "utf8");
+  const decryptedMessage = ASCIIdecryptor(encryptedMessage);
+  console.log(decryptedMessage);
+} catch (error) {
+  console.error(`ERROR: ${error}`);
 }
 
-main();
 export default main;
