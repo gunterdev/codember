a3ea0091622c74074192f3c3c573d7259924446e
Eric Clemente <eeric20011@gmail.com>
2022-11-30 01:59:33 +0100

    Update: Better names of variables

--- challenge05/hunger-games.js
+++ challenge05/hunger-games.js
@@ -3,15 +3,15 @@ export const hungerGames = (players) => {
   if (!Array.isArray(players)) throw new TypeError(`${typeof players} is not an array`);
   if (players.length === 0) return null;
   // Actual logic
-  let alivePlayersPositions = [...players.keys()];
-  let startPositionOfFirstKiller = 0;
-  while (alivePlayersPositions.length > 1) {
-    let lastAlivePlayer = alivePlayersPositions.at(-1);
-    alivePlayersPositions = alivePlayersPositions.filter((_, a) => a % 2 === startPositionOfFirstKiller);
-    startPositionOfFirstKiller = lastAlivePlayer === alivePlayersPositions.at(-1) ? 1 : 0;
+  let alivePlayersIndex = [...players.keys()];
+  let startingIndexOfFirstKiller = 0;
+  while (alivePlayersIndex.length > 1) {
+    let lastAlivePlayer = alivePlayersIndex.at(-1);
+    alivePlayersIndex = alivePlayersIndex.filter((_, a) => a % 2 === startingIndexOfFirstKiller);
+    startingIndexOfFirstKiller = lastAlivePlayer === alivePlayersIndex.at(-1) ? 1 : 0;
   }
   // Extract the winner
-  const [winnerPosition] = alivePlayersPositions;
+  const [winnerIndex] = alivePlayersIndex;
   // Return the winner name and index position
-  return [players[winnerPosition], winnerPosition];
+  return [players[winnerIndex], winnerIndex];
 };
